a9e87b93777d96e2cb58ce909f91ea09c6ec8340
eksalp <eksalpu0@gmail.com>
2023-03-19 09:44:33 +0700

    update teross

--- src/components/container/Footer.jsx
+++ src/components/container/Footer.jsx
@@ -9,7 +9,7 @@ import logooo from '../../assets/fixlogoputih.png';
 const Footer = () => {
   return (
     <motion.div initial={{ height: 0 }} whileInView={{ height: 'auto' }} transition={{ duration: 0 }} className="bg-[#ff0000] p-10">
-      <div className="grid md:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 place-items-start gap-8 text-white">
+      <div className="grid md:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 place-items-start gap-10 text-white">
         <div>
           <div className="w-[150px] mb-3">
             <img src={logooo} alt="" />
@@ -17,9 +17,8 @@ const Footer = () => {
           <p className="text-sm leading-7 mb-1">Alamat Kantor: Asrama UNS, Jebres, Kota Surakarta, Jawa Tengah 57126.</p>
 
           <iframe
-            className="rounded-xl drop-shadow-md"
+            className="rounded-xl drop-shadow-md w-[335px] md:w-[250px]"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1175.8820342388976!2d110.86591960718316!3d-7.555348502303528!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a171ac7a9a63b%3A0x270a5a229ac41dbe!2sAsrama%20Mahasiswa%20UNS!5e0!3m2!1sen!2sid!4v1676935833415!5m2!1sen!2sid"
-            width="300"
             height="200"
             loading="lazy"
             referrerpolicy="no-referrer-when-downgrade"
@@ -94,7 +93,7 @@ const Footer = () => {
         </div>
       </div>
       <br />
-      <div className="text-white justify-center text-center">Copyright © 2023 OTOranger. All Rights Reserved</div>
+      <div className="text-white justify-center text-center text-sm">Copyright © 2023 OTOranger. All Rights Reserved</div>
     </motion.div>
   );
 };
